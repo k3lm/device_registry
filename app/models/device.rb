a5daea74e8031bd351e2a0aa7c08c5f867a66f6c
Mateusz Mach <matmach99@gmail.com>
2024-10-09 15:57:48 +0200

    Update models to include relationships between them

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,5 @@
 class Device < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :returned_devices
+  has_many :users_returned, through: :returned_devices, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
   has_secure_password
+  has_many :devices
+  has_many :returned_devices
+  has_many :devices_returned, through: :returned_devices, source: :device
 end
