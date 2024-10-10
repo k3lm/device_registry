b31250e1333ab3bec44bc6750b23166743810a0e
Mateusz Mach <matmach99@gmail.com>
2024-10-10 12:11:46 +0200

    Update device and user model with validations and associations

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,7 @@
 class Device < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :returned_devices
+  has_many :returned_devices, dependent: :destroy
   has_many :users_returned, through: :returned_devices, source: :user
+
+  validates :serial_number, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
   has_secure_password
-  has_many :devices
-  has_many :returned_devices
+  has_many :devices, dependent: :nullify
+  has_many :returned_devices, dependent: :destroy
   has_many :devices_returned, through: :returned_devices, source: :device
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
