550e174f4ebfc1f8cb7f3440c5628ed6f25a7285
Mateusz Mach <matmach99@gmail.com>
2024-10-10 18:35:05 +0200

    Refactor RSpec tests for AssignDeviceToUser

--- spec/services/assign_device_to_user_spec.rb
+++ spec/services/assign_device_to_user_spec.rb
@@ -3,22 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe AssignDeviceToUser do
-  subject(:assign_device) do
-    described_class.new(
-      requesting_user: user,
-      serial_number: serial_number,
+  let(:user) { create(:user) }
+  let(:serial_number) { '123456' }
+
+  def assign_device(requesting_user: user, device_serial_number: serial_number, new_device_owner_id: user.id)
+    AssignDeviceToUser.new(
+      requesting_user: requesting_user,
+      serial_number: device_serial_number,
       new_device_owner_id: new_device_owner_id
     ).call
   end
 
-  let(:user) { create(:user) }
-  let(:serial_number) { '123456' }
-
-  context 'when users registers a device to other user' do
+  context 'when user registers a device to another user' do
     let(:new_device_owner_id) { create(:user).id }
 
-    it 'raises an error' do
-      expect { assign_device }.to raise_error(RegistrationError::Unauthorized)
+    it 'raises an unauthorized error' do
+      expect {
+        assign_device(new_device_owner_id: new_device_owner_id)
+      }.to raise_error(RegistrationError::Unauthorized)
     end
   end
 
@@ -38,23 +40,23 @@ RSpec.describe AssignDeviceToUser do
       end
 
       it 'does not allow to register' do
-        expect { assign_device }.to raise_error(AssigningError::AlreadyUsedOnUser)
+        expect {
+          assign_device
+        }.to raise_error(AssigningError::AlreadyUsedOnUser)
       end
     end
 
-    context 'when user tries to register device that is already assigned to other user' do
+    context 'when user tries to register a device that is already assigned to another user' do
       let(:other_user) { create(:user) }
 
       before do
-        AssignDeviceToUser.new(
-          requesting_user: other_user,
-          serial_number: serial_number,
-          new_device_owner_id: other_user.id
-        ).call
+        assign_device(requesting_user: other_user, new_device_owner_id: other_user.id)
       end
 
       it 'does not allow to register' do
-        expect { assign_device }.to raise_error(AssigningError::AlreadyUsedOnOtherUser)
+        expect {
+          assign_device
+        }.to raise_error(AssigningError::AlreadyUsedOnOtherUser)
       end
     end
   end
